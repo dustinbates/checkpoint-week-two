19e18acd57f80a01cd71835283d1a29a5fc58acb
dustinbates <dustin.bates98@gmail.com>
2023-01-28 00:40:36 -0700

    all requirements met (technically), need cost of upgrades to be drawn on screen

--- app.js
+++ app.js
@@ -33,13 +33,14 @@ let autoUpgrades = [
 let joyTotal = 0
 let rainbowTotal = 0
 let magicTotal = 0
+let doubleRainbowTotal = 0
+let friendshipTotal = 0
 
 // SECTION functions for the game
 
 // NOTE increases the currency per each click
 function increaseJoy(){
     console.log("You clicked it")
-    // debugger
     clickUpgrades.forEach(upgrade => {
         let total = (upgrade.multiplier * upgrade.quantity)
         console.log();
@@ -68,25 +69,39 @@ function updateRainbows(){
     rainbow.innerText = rainbowTotal
 }
 
+function updateDoubleRainbows(){
+    let rainbow = document.getElementById('totalDoubleRainbows')
+    rainbow.innerText = doubleRainbowTotal
+}
+
 function updateMagic(){
     let magic = document.getElementById('totalMagic')
     magic.innerText = magicTotal
 }
 
+function updateFriendship(){
+    let friendship = document.getElementById('totalFriendship')
+    friendship.innerText = friendshipTotal
+}
+
 function updatePerClick(){
+    let perClick = 0
     let totalPerClick = document.getElementById('totalPerClick')
     clickUpgrades.forEach(upgrade => {
         let total = upgrade.multiplier * upgrade.quantity
-        totalPerClick.innerText = total + 1
+        perClick += total
     })
+    totalPerClick.innerText = perClick + 1
 }
 
 function updateAutoClick(){
+    let autoClick = 0
     let totalAutoClick = document.getElementById('totalAutoClick')
     autoUpgrades.forEach(upgrade => {
         let total = upgrade.multiplier * upgrade.quantity
-        totalAutoClick.innerText = total
+        autoClick += total
     })
+    totalAutoClick.innerText = autoClick
 }
 
 // NOTE purchases an upgrade
@@ -106,6 +121,22 @@ function buyRainbow(){
     }
 }
 
+function buyDoubleRainbow(){
+    let rainbow = clickUpgrades.find(i => i.name == 'Double Rainbows')
+    if(joyTotal >= rainbow.price){
+        console.log("You got it")
+        doubleRainbowTotal++
+        rainbow.quantity++
+        joyTotal -= rainbow.price
+        rainbow.price += 100
+        updateDoubleRainbows()
+        updateJoy()
+        updatePerClick()
+    } else {
+        window.alert("You need more Joy!")
+    }
+}
+
 function buyMagic(){
     let magic = autoUpgrades.find(i => i.name == 'Magic')
     if(joyTotal >= magic.price){
@@ -116,6 +147,22 @@ function buyMagic(){
         joyTotal -= magic.price - 50
         updateJoy()
         updateMagic()
+        updateAutoClick()
+    } else {
+        window.alert("You need more Joy!");
+    }
+}
+
+function buyFriendship(){
+    let friendship = autoUpgrades.find(i => i.name == 'Friendship')
+    if(joyTotal >= friendship.price){
+        console.log("You got it");
+        friendshipTotal++
+        friendship.quantity++
+        friendship.price += 500
+        joyTotal -= friendship.price - 500
+        updateJoy()
+        updateFriendship()
         updateAutoClick()
     } else {
         window.alert("You need more Joy!");
